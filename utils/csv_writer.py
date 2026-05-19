e1bfa7183ba4a399078b646870669df416c32834
Swapnil Gilbile <143521819+Swapnil7494@users.noreply.github.com>
2026-05-19 19:58:34 +0530

    Create csv_writer.py

---
+++ utils/csv_writer.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import os
+
+OUTPUT_DIR = "output"
+MASTER_FILE = "all_bills.csv"
+
+COLUMNS = [
+    "file_name",
+    "consumer_id",
+    "name",
+    "billing_date",
+    "units",
+    "amount",
+    "due_date"
+]
+
+
+def append_to_csv(data, file_name=None):
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+    file_path = os.path.join(OUTPUT_DIR, MASTER_FILE)
+
+    if "error" in data:
+        return None
+
+    data["file_name"] = file_name
+
+    new_df = pd.DataFrame([data])
+
+    for col in COLUMNS:
+        if col not in new_df.columns:
+            new_df[col] = None
+
+    new_df = new_df[COLUMNS]
+
+    if os.path.exists(file_path):
+        existing_df = pd.read_csv(file_path)
+
+        updated_df = pd.concat([existing_df, new_df], ignore_index=True)
+
+        updated_df = updated_df.drop_duplicates(
+            subset=["file_name", "consumer_id", "billing_date"],
+            keep="last"
+        )
+    else:
+        updated_df = new_df
+
+    updated_df.to_csv(file_path, index=False)
+
+    return file_path
